8bfaff575fb81a1f2b6d99396e423ef772ea2cf7
Fateehs <benzer1010@gmail.com>
2023-08-02 13:35:29 +0300

    Register Feature added

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -6,20 +6,31 @@ import {
   ValidationErrors,
   Validators,
 } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.components';
 import { Create_User } from 'src/app/contracts/operation-result/create_user_result';
 import { User } from 'src/app/entities/user';
 import { UserService } from 'src/app/services/common/models/user.service';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+  ToastrPosition,
+} from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent extends BaseComponent implements OnInit {
   constructor(
     private userService: UserService,
-    private formBuilder: FormBuilder
-  ) {}
+    private formBuilder: FormBuilder,
+    private toastrService: CustomToastrService,
+    spinner: NgxSpinnerService
+  ) {
+    super(spinner);
+  }
 
   registerFormGroup: FormGroup;
   ngOnInit(): void {
@@ -46,13 +57,24 @@ export class RegisterComponent implements OnInit {
 
   submitted: boolean = false;
   async onSubmit(user: User) {
+    this.showSpinner(SpinnerType.BallBeat);
     this.submitted = true;
-    debugger;
 
     if (this.registerFormGroup.invalid) return;
 
     const result: Create_User = await this.userService.create(user);
-    if (result.succeeded) result.message, 'User registration successful.';
-    else result.message, 'User registration not successful.';
+    if (result.succeeded) {
+      this.toastrService.message(result.message, 'Account Created', {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight,
+      });
+    } else {
+      this.toastrService.message(result.message, 'Something is wrong', {
+        messageType: ToastrMessageType.Error,
+        position: ToastrPosition.TopRight,
+      });
+    }
+
+    this.hideSpinner(SpinnerType.BallBeat);
   }
 }
