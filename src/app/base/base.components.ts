123f4cd0758df93bc04a8122c42ea833c55eb22e
Fateehs <benzer1010@gmail.com>
2023-08-02 13:34:29 +0300

    Base Component created

---
+++ src/app/base/base.components.ts
@@ -0,0 +1,19 @@
+import { NgxSpinnerService } from "ngx-spinner";
+
+export class BaseComponent {
+    constructor(public spinner: NgxSpinnerService) { }
+  
+    showSpinner(spinnerType: SpinnerType) {
+      this.spinner.show(spinnerType);
+      setTimeout(() => this.hideSpinner(spinnerType), 3000)
+    }
+  
+    hideSpinner(spinnerType: SpinnerType) {
+      this.spinner.hide(spinnerType);
+      setTimeout(() => this.hideSpinner(spinnerType), 3000)
+    }
+  }
+  
+  export enum SpinnerType {
+    BallBeat = "s1",
+  }
