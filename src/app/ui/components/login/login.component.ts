c9e1727cd1f292a42892d4378876efdff275046e
Fateehs <benzer1010@gmail.com>
2023-08-02 13:35:15 +0300

    Login Feature added

--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.components';
+import { User } from 'src/app/entities/user';
+import {
+  AuthService,
+  _isAuthenticated,
+} from 'src/app/services/common/auth.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
+import { UserAuthService } from 'src/app/services/common/models/user-auth.service';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+  ToastrPosition,
+} from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent extends BaseComponent implements OnInit {
+  constructor(
+    private userAuthService: UserAuthService,
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private toastrService: CustomToastrService,
+    spinner: NgxSpinnerService
+  ) {
+    super(spinner);
+  }
 
+  loginFormGroup: FormGroup;
+  ngOnInit(): void {
+    this.loginFormGroup = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  async login(user: User) {
+    this.showSpinner(SpinnerType.BallBeat);
+    await this.userAuthService.login(user.username, user.password, () => {
+      this.authService.identityCheck();
+      if (_isAuthenticated) {
+        this.toastrService.message('Login Successful', 'You logged in', {
+          messageType: ToastrMessageType.Success,
+          position: ToastrPosition.TopRight,
+        });
+      }
+
+      this.activatedRoute.queryParams.subscribe((params) => {
+        const returnUrl: string = params['returnUrl'];
+        if (returnUrl) this.router.navigate([returnUrl]);
+      });
+      this.hideSpinner(SpinnerType.BallBeat);
+    });
+  }
 }
